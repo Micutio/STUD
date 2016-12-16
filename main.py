76ca6a18511865013b54d145372ee351e99b615e
mWagner <michael.wagner@tum-create.edu.sg>
2016-12-16 18:08:37 +0800

    Added: simple error handling, few tweaks in output

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ Updates:
 import json
 import urllib
 import time
+import traceback
 from geopy.geocoders import Nominatim
 
 __author__ = 'Michael Wagner'
@@ -16,6 +17,8 @@ __author__ = 'Michael Wagner'
 def startup():
     print('>>> S.T.U.D. - Space Telemetry')
 
+def init_geolocator():
+    return Nominatim()
 
 def get_iss_loc():
     # Get json data with current location of ISS
@@ -26,8 +29,7 @@ def get_iss_loc():
     return data_dict['iss_position']['latitude'], data_dict['iss_position']['longitude']
 
 
-def get_location_for_coords(lat, lon):
-    geolocator = Nominatim()
+def get_location_for_coords(lat, lon, geolocator):
     location = geolocator.reverse(lat + ', ' + lon)
     # print(location.raw)
     return location.address
@@ -35,10 +37,19 @@ def get_location_for_coords(lat, lon):
 
 if __name__ == '__main__':
     startup()
+    geolocator = init_geolocator()
     while True:
-        iss_lat, iss_lon = get_iss_loc()
-        iss_add = get_location_for_coords(iss_lat, iss_lon)
-        output = 'ISS location >>> Lat: {0}, Lon: {1}\n'\
-                 'approximate to {2}'.format(iss_lat, iss_lon, iss_add)
-        print(output)
+        try:
+            # TODO: handle errors when geocoder unavailable
+            iss_lat, iss_lon = get_iss_loc()
+            iss_add = get_location_for_coords(iss_lat, iss_lon, geolocator)
+            
+            # TODO: Only print the second line when iss_add not None!
+            print('ISS location >>> Lat: {0}, Lon: {1}'.format(iss_lat, iss_lon))
+            if iss_add is not None:
+                print('above: {0}'.format(iss_add))
+
+        except:
+            print('Error: Service currently unavailable. Trying again later')
+            traceback.print_exc()
         time.sleep(30)
